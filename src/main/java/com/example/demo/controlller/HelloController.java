59ed8605c3a8d0b6e486948dddceca1c05a83ff8
Arjun-steam <Arjun@steam-a.com>
2025-02-26 23:25:30 +0530

    first commit

---
+++ src/main/java/com/example/demo/controlller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @Value("${custom.message}")
+    private String message;
+
+    @GetMapping("/info")
+    public String getInfo() {
+        return "Active Profile: " + message;
+    }
+}
+
